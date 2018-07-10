8d2abb16014996a35b251c8b588af6384c38af92
haruny <harunyayli@gmail.com>
2018-07-09 19:32:37 -0700

    device support

--- adt-pulse-mqtt/server.js
+++ adt-pulse-mqtt/server.js
@@ -9,6 +9,8 @@ var alarm_command_topic = config.alarm_command_topic;
 var zone_state_topic = config.zone_state_topic;
 
 var alarm_last_state = "unknown";
+var devices = {};
+
 
 // Register Callbacks:
 myAlarm.onDeviceUpdate(
@@ -21,7 +23,6 @@ myAlarm.onStatusUpdate(
   function(device) {
       var mqtt_state = "unknown";
       var status = device.status.toLowerCase();
-      var logtime = new Date();
 
       if (status.includes('disarmed')) {
           mqtt_state = "disarmed";
@@ -40,7 +41,7 @@ myAlarm.onStatusUpdate(
       }
 
       if (!mqtt_state.includes(alarm_last_state) && !mqtt_state.includes('unknown')) {
-         console.log(logtime.toLocaleString()+": Pushing alarm state: "+mqtt_state+" to "+alarm_state_topic);
+         console.log((new Date()).toLocaleString()+": Pushing alarm state: "+mqtt_state+" to "+alarm_state_topic);
          client.publish(alarm_state_topic, mqtt_state,{"retain":"true"});
          alarm_last_state = mqtt_state;
       }
@@ -49,13 +50,25 @@ myAlarm.onStatusUpdate(
 
 myAlarm.onZoneUpdate(
   function(device) {
-    console.log(logtime.toLocaleString()+": Zone state: receieved '"+ device + "' will push to "+ zone_state_topic);
+    if (devices[device.id]==null){
+        console.log ((new Date()).toLocaleString()+" New device: "+ device.id);
+    }
+
+    dev_zone_state_topic = zone_state_topic+"/"+device.name+"/state";
+
+    if (devices[device.id]==null || device.activityTs!=devices[device.id].activityTs){
+        console.log((new Date()).toLocaleString()+": Zone state: receieved '"+ device.id + "' will push to "+ dev_zone_state_topic+" "+ JSON.stringify(device));
+        client.publish(dev_zone_state_topic, JSON.stringify(device),{"retain":"true"});
+	  }
+      else{
+        console.log((new Date()).toLocaleString()+" Zone "+device.id+ " din't change. Skipping");
+      }
+      devices[device.id] = device;
   }
 )
 
 client.on('connect', () => {
-  var logtime = new Date();
-  console.log(logtime.toLocaleString()+': Connected successfully to mqtt');
+  console.log((new Date()).toLocaleString()+': Connected successfully to mqtt');
 })
 
 // Login - gets all devices, zones and status and invokes callbacks:
