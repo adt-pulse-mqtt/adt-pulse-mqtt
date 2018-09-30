30f2d26ad085fd7eee9828dc19d39b1c837149ec
Craig Leikis <digitalcraig@users.noreply.github.com>
2018-09-30 15:48:46 -0500

    Base image default
    Set base image to default in Docker file and add comments in docker-build.sh on how to build a local image from a different base image.

--- docker-build.sh
+++ docker-build.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 
 cd adt-pulse-mqtt
-docker build --build-arg BUILD_FROM="homeassistant/amd64-base:latest" -t local/adt-pulse-mqtt .
+
+# Default build is based on homeassistant/amd64-base:latest
+docker build -t local/adt-pulse-mqtt .
+
+# To specify a different platform, set build-arg to a different base image:
+# homeassistant/armhf-base
+# homeassistant/amd64-base
+# homeassistant/aarch64-base
+# homeassistant/i386-base
+
+#docker build --build-arg BUILD_FROM="homeassistant/amd64-base:latest" -t local/adt-pulse-mqtt .
+
+
