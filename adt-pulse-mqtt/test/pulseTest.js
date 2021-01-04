66fa1887377e050bbfa3f127570afca4f97c97e7
Craig Leikis <cleikis@gmail.com>
2021-01-04 17:35:23 -0600

    Badges for Travis  CI and  Coveralls

---
+++ adt-pulse-mqtt/test/pulseTest.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const rewire = require('rewire');
+const expect = require('chai').expect;
+
+describe('Pulse - Main class', () => {
+    let module = rewire('../adt-pulse.js');
+  });
